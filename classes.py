7d7cb433eea5a47e782beaab750a6867932f76bf
Matheus <matheus.catarucci7@gmail.com>
2025-09-09 10:21:31 -0300

    Função Locadora feita

--- classes.py
+++ classes.py
@@ -1,3 +1,28 @@
 class Locadora:
-    def __init__(self, clientes, itens):
-        self.__
+    # Método construtor
+    def __init__(self):
+        self.__clientes = []
+        self.__itens = []
+
+    # Métodos
+    def cadastrarCliente(self, cliente):
+        self.__clientes.append(cliente)
+        print("Cliente Cadastrado com sucesso!")
+    
+    def cadastrarItem(self, item):
+        self.__itens.append(item)
+        print("Item cadastrado com sucesso!")
+
+    def listarClientes(self):
+        if not self.__clientes:
+            print("Nenhum cliente cadastrado")
+        else:            
+            for cliente in self.__clientes:
+                print(f"Cliente: {cliente}")
+
+    def listarItens(self):
+        if not self.__itens:
+            print("Nenhum item cadastrado")
+        else:
+            for item in self.__itens:
+                print(f"Item: {item}")
